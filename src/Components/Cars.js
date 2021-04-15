6dc82f6ef3f419fd97f9677aede50fb38498c48e
SahilRajpal-hub <sahilrajpal.31586@gmail.com>
2021-04-15 12:01:20 +0530

    Loader setuped

--- src/Components/Cars.js
+++ src/Components/Cars.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import firebase from './../firebase/firebase.utils'
+import Loader from './Loader'
 
 const Cars = () => {
   const [societies, setSocieties] = useState([])
   const [areas, setAreas] = useState([])
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     firebase
@@ -13,15 +15,19 @@ const Cars = () => {
         'value',
         (snapshot) => {
           const a = []
+          const s = []
           snapshot.forEach((element) => {
             if (element.key !== 'clusters') {
               // element.key -> area name
               // element.val() -> societies Object
               console.log(element.key + ' : ' + element.val())
               a.push(element.key)
+              s.push(element.val())
             }
           })
           setAreas(a)
+          setSocieties(s)
+          setLoading(false)
         },
         (err) => {
           console.log(err)
@@ -32,9 +38,12 @@ const Cars = () => {
     <div className='main-content' id='panel'>
       <div className='container-fluid'>
         <h1>Cars</h1>
-        {areas.map((area) => (
-          <h1>{area}</h1>
-        ))}
+        {loading ? <Loader /> : areas.map((area) => <h1>{area}</h1>)}
+        {loading ? (
+          <Loader />
+        ) : (
+          societies.map((society) => <h1>{Object.keys(society)[0]}</h1>)
+        )}
       </div>
     </div>
   )
