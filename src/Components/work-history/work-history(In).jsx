d095fb37bded7b5a428637b8d728cb7d14fb9ae2
Sandeep Chaudhary <sandee.chaudhary367@gmail.com>
2021-06-03 13:43:22 +0530

    calender added

--- src/Components/work-history/work-history(In).jsx
+++ src/Components/work-history/work-history(In).jsx
@@ -1,71 +1,55 @@
 import React ,{useEffect,useState}from "react";
 import firebase from '../../firebase/firebase.utils.js'
-
-
+import 'react-calendar/dist/Calendar.css';
+import Calendar from 'react-calendar';
+import "./work-history.css"
 
 const WorkHistory = ({carnum})=>{
-    const [Dat, setDate] = useState([]);
-    const [Prove, setProve] = useState({});
-    const [loading, setLoading] = useState(true)
+  const [value, onChange] = useState(new Date());
 
-    const Historyfetch=function(){
-         firebase
-          .database()
-          .ref(`Car Status/${carnum}/Interior Work History`)
-          .on(
-            'value',
-            (snapshot) => {
-                const a = []
-                const s = []
-            snapshot.forEach((element) => {
-                a.push(element.key)
-                s.push(element.val())
-          })
-          setDate(a)
-          setProve(s)
-          setLoading(false)
-            },
-            (err) => {
-              console.log(err)
-            }
-          )
-      }
-    
-      
+  const showImage=(value,e)=>{
+     const date= `${value.getFullYear()}-${ String(value.getMonth()+1).padStart(2, '0')}-${String(value.getDate()).padStart(2, '0')}`;
+     let imageURL=null;
+     firebase
+   .database()
+   .ref(`Car Status/DLDEMO1/Interior Work History/${date}/Photo Url`)
+   .on(
+     'value',
+     (snapshot) => {
+        imageURL= snapshot.val();
 
-    useEffect(() =>{
-      Historyfetch() },[])
-
-      return(
-            <div>
-            {loading ? null:
-            <div class="card">
-            <div class="card-body">
-                <h3>Work History-Interior</h3>
-                <table className="table table-sm table-bordered table-responsive-xl ">
-                    <thead className="bg-dark">
-                        <tr >
-                            <th style={{fontSize:15,color:'white',textAlign:"center"}}>Date</th>
-                            <th style={{fontSize:15,color:'white',textAlign:"center"}}>Image Url</th>
-                            <th style={{fontSize:15,color:'white',textAlign:"center"}}>Done by</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        
-                            {Dat.map((element,i)=>(
-                            <tr style={{fontSize:15,color:'black',textAlign:"center"}} >
-                            <td >{element}</td>
-                            <td >{Prove[i].PhotoUrl0}</td>
-                            <td >{Prove[i].doneBy}</td>
-                            </tr>
-                            ))} 
-                    </tbody>
-                </table>
-                </div>
-                </div>
-            }
-            </div>
-        )
+     })
+     if(imageURL){
+      window.open(imageURL, '_blank', 'noopener,noreferrer')
+     }
+ // console.log(`Car Status/${carnum}/Work History/${date}/Photo Url`)
+  }
+ 
+ const yy=({ activeStartDate, date, view }) => {
+     const dat= `${date.getFullYear()}-${ String(date.getMonth()+1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`;
+     let img=0;
+     firebase
+   .database()
+   .ref(`Car Status/DLDEMO1/Interior Work History/${dat}/Photo Url`)
+   .on(
+     'value',
+     (snapshot) => {
+        img= snapshot.val();
+     })
+     return img?"clean":null
+ }
+ return (
+<div style={{margin:"20px 0"}}>
+ <Calendar 
+ className="clean full"
+ onClickDay={showImage}
+ onChange={onChange}
+ view='month'
+ tileClassName={yy}
+ value={value}
+  />
+  </div>
+ )
 }
 
 export default WorkHistory;
